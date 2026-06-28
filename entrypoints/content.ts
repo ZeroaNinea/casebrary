e182a5c219e24073d434c7223c7e36fdd2b11e89
Catgirl <zeroaninea@gmail.com>
2026-06-28 22:56:23 +0400

    playing with indexeddb

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,27 @@
+import { dbPromise } from '../database/db';
+import Entry, { EntryType } from '../types/entry.interface';
+
 export default defineContentScript({
   matches: ['*://*.google.com/*'],
-  main() {
+  async main() {
     console.log('Hello content.');
+
+    const db = await dbPromise;
+
+    await db.put('entries', {
+      id: '1',
+      title: 'Hello',
+      parentId: null,
+      type: EntryType.Note,
+      properties: {},
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    } as Entry);
+
+    console.log(await db.get('entries', '1'));
+
+    console.log(await db.getAll('entries'));
+
+    await db.delete('entries', '1');
   },
 });
