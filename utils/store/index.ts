2590e5788fff0cf507c118c0113429b2e8e033a2
Catgirl <zeroaninea@gmail.com>
2026-06-30 22:28:59 +0400

    put the reducers into an object

--- utils/store/index.ts
+++ utils/store/index.ts
@@ -2,5 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import entriesSlice from '@/features/entries/entries.slice';
 
 export default configureStore({
-  reducer: entriesSlice.reducer,
+  reducer: {
+    entries: entriesSlice.reducer,
+  },
 });
