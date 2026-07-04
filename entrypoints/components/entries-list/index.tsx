52681c91215ab559276fb6b299352693e4345d22
Catgirl <zeroaninea@gmail.com>
2026-07-05 01:29:42 +0400

    renderIcon added

--- entrypoints/components/entries-list/index.tsx
+++ entrypoints/components/entries-list/index.tsx
@@ -1,10 +1,25 @@
 import Entry from '@/types/entry.interface';
+import { icons } from '@/utils/icons';
 
 export default function EntriesList({ entries }: { entries: Entry[] }) {
+  function renderIcon(entry: Entry) {
+    if (!entry.icon) {
+      return null;
+    } else if (entry.icon.type === 'lucide') {
+      const Icon = icons[entry.icon.value];
+      return <Icon size={18} />;
+    } else {
+      return (
+        <img src={entry.icon.value} width={18} height={18} alt={entry.title} />
+      );
+    }
+  }
+
   return (
     <ul>
       {entries.map((entry) => (
         <li key={entry.id}>
+          {renderIcon(entry)}
           <h1>{entry.title}</h1>
         </li>
       ))}
