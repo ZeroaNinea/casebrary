7b1b7ee435d92811c83d7d3b1567974d55c4d542
Catgirl <zeroaninea@gmail.com>
2026-07-07 20:40:39 +0400

    my added

--- entrypoints/components/pages/property-editor/properties-list.tsx
+++ entrypoints/components/pages/property-editor/properties-list.tsx
@@ -1,8 +1,10 @@
 import { Property, PropertyType } from '@/types/entry.interface';
 
 import RippleButton from '@/entrypoints/components/buttons/ripple-button';
+import ClassicIconButton from '@/entrypoints/components/buttons/classic-icon-button';
 
 import { propertyIcons } from '@/utils/property';
+import { EllipsisVertical } from 'lucide-react';
 
 export default function ({ properties }: { properties: Property[] }) {
   function renderIcon(type: PropertyType) {
@@ -12,32 +14,53 @@ export default function ({ properties }: { properties: Property[] }) {
 
   return (
     <div className="flex flex-col gap-2">
-      {/* {properties.map((property) => (
-        <div key={property.id} className="px-3 py-3">
-          <div className="flex items-center gap-2">
-            <span className="text-primary-on-container">{property.name}</span>
-            <span className="text-primary-on-container">{property.type}</span>
-            <span className="text-primary-on-container">{property.value}</span>
-          </div>
-        </div>
-      ))} */}
+      {/* {JSON.stringify(properties)} */}
       {properties.map((property) => (
-        <div key={property.id} className="px-3 py-3">
-          <div className="flex items-center gap-2">
+        <div
+          key={property.id}
+          className="flex justify-between items-center px-3 py-3"
+        >
+          <div>
             <div className="flex items-center gap-2">
-              {renderIcon(property.type)}
-              <h1 className="text-primary-title">{property.name}</h1>
+              <div className="flex items-center gap-2">
+                {renderIcon(property.type)}
+                <h1 className="text-primary-title">{property.name}</h1>
+              </div>
             </div>
             <RippleButton
               mode="dark"
               className="
-                px-2 py-1 rounded-md border-berder
-                hover:border-primary-container-hover transition-all duration-200
+                px-2 py-1 text-sm rounded-md border border-border
+                hover:bg-primary-container-hover transition-all duration-200
               "
             >
               <span className="text-primary-title">{property.type}</span>
             </RippleButton>
-            <p>{property.value}</p>
+            <p className="my-2">{property.value}</p>
+          </div>
+          <div className="relative">
+            <ClassicIconButton
+              title="Options"
+              isState={true}
+              // onClick={() => {
+              //   if (isDropdownOpenId === entry.id) {
+              //     setIsDropdownOpenId(null);
+              //   } else {
+              //     setIsDropdownOpenId(entry.id);
+              //   }
+              // }}
+            >
+              <EllipsisVertical
+                size={18}
+                // style={
+                //   {
+                //     '--primary-title': entry.color && palettes[entry.id]['900'],
+                //   } as React.CSSProperties
+                // }
+                color="var(--color-primary-title)"
+              />
+            </ClassicIconButton>
+            {/* {renderDropdown(entry)} */}
           </div>
         </div>
       ))}
