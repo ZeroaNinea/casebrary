706faf7de54094ac6c20f24f847b3b2cf070c90c
Catgirl <zeroaninea@gmail.com>
2026-07-07 20:12:14 +0400

    properties list updated

--- entrypoints/components/pages/property-editor/properties-list.tsx
+++ entrypoints/components/pages/property-editor/properties-list.tsx
@@ -1,9 +1,18 @@
-import { Property } from '@/types/entry.interface';
+import { Property, PropertyType } from '@/types/entry.interface';
+
+import RippleButton from '@/entrypoints/components/buttons/ripple-button';
+
+import { propertyIcons } from '@/utils/property';
 
 export default function ({ properties }: { properties: Property[] }) {
+  function renderIcon(type: PropertyType) {
+    const Icon = propertyIcons[type];
+    return <Icon size={16} />;
+  }
+
   return (
-    <>
-      {properties.map((property) => (
+    <div className="flex flex-col gap-2">
+      {/* {properties.map((property) => (
         <div key={property.id} className="px-3 py-3">
           <div className="flex items-center gap-2">
             <span className="text-primary-on-container">{property.name}</span>
@@ -11,7 +20,27 @@ export default function ({ properties }: { properties: Property[] }) {
             <span className="text-primary-on-container">{property.value}</span>
           </div>
         </div>
+      ))} */}
+      {properties.map((property) => (
+        <div key={property.id} className="px-3 py-3">
+          <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2">
+              {renderIcon(property.type)}
+              <h1 className="text-primary-title">{property.name}</h1>
+            </div>
+            <RippleButton
+              mode="dark"
+              className="
+                px-2 py-1 rounded-md border-berder
+                hover:border-primary-container-hover transition-all duration-200
+              "
+            >
+              <span className="text-primary-title">{property.type}</span>
+            </RippleButton>
+            <p>{property.value}</p>
+          </div>
+        </div>
       ))}
-    </>
+    </div>
   );
 }
---
+++ utils/property.ts
@@ -0,0 +1,17 @@
+import {
+  Calendar,
+  CheckSquare,
+  FileText,
+  Hash,
+  Image,
+  Link,
+} from 'lucide-react';
+
+export const propertyIcons = {
+  text: FileText,
+  number: Hash,
+  boolean: CheckSquare,
+  date: Calendar,
+  url: Link,
+  image: Image,
+};
